2ec62a5836705355a6f4e6813f8b82f3082c9532
陈波 <innerpeacer@gmail.com>
2014-12-27 12:20:53 +0800

    Adding the support for dealing with .mtl file

--- blender2opengles/main.cpp
+++ blender2opengles/main.cpp
@@ -20,6 +20,7 @@ typedef struct Model {
     int texels;
     int normals;
     int faces;
+    int materials;
 } Model;
 
 Model getOBJinfo(string fp)
@@ -57,13 +58,14 @@ Model getOBJinfo(string fp)
     return model;
 }
 
-void extractOBJData(string fp, float positions[][3], float texels[][2], float normals[][3], int faces[][9])
+void extractOBJData(string fp, float positions[][3], float texels[][2], float normals[][3], int faces[][10], string *materials, int m)
 {
     int p = 0;
     int t = 0;
     int n = 0;
     int f = 0;
     
+    int mtl = 0;
     
     ifstream inOBJ;
     inOBJ.open(fp);
@@ -77,6 +79,18 @@ void extractOBJData(string fp, float positions[][3], float texels[][2], float no
         string line;
         getline(inOBJ, line);
         string type = line.substr(0, 2);
+        
+        if (type.compare("us") == 0) {
+            string l = "usemtl ";
+            string material = line.substr(l.size());
+            
+            for (int i = 0; i < m; i++) {
+                if (material.compare(materials[i]) == 0) {
+                    mtl = i;
+                }
+            }
+        }
+        
         
         if (type.compare("v ") == 0) {
             char *l = new char[line.size() + 1];
@@ -124,6 +138,8 @@ void extractOBJData(string fp, float positions[][3], float texels[][2], float no
                 faces[f][i] = atof(strtok(NULL, " /"));
             }
             
+            faces[f][9] = mtl;
+            
             delete [] l;
             f++;
         }
@@ -150,12 +166,22 @@ void writeH(string fp, string name, Model model)
     outH << "// Normals: " << model.normals << endl;
     outH << "// Faces: " << model.faces << endl;
     outH << "// Vertices: " << model.vertices << endl;
-
+    outH << "// Materials: " << model.materials << endl;
+    outH << endl;
     
     outH << "const int " << name << "Vertices;" << endl;
     outH << "const float " << name << "Positions[" << model.vertices * 3 << "];" <<endl;
     outH << "const float " << name << "Texels[" << model.vertices * 2 << "];" << endl;
     outH << "const float " << name << "Normals[" << model.vertices * 3 << "];" << endl;
+    outH << endl;
+    
+    outH << "const int " << name << "Materials;" << endl;
+    outH << "const int " << name << "Firsts[" << model.materials << "];" << endl;
+    outH << "const int " << name << "Counts[" << model.materials << "];" << endl;
+    outH << endl;
+    
+    outH << "const float " << name << "Diffuses[" << model.materials << "]" << "[" << 3 << "];" <<endl;
+    outH << "const float " << name << "Speculars[" << model.materials << "]" << "[" << 3 << "];" << endl;
     outH << endl;
 
     outH.close();
@@ -183,7 +209,7 @@ void writeCvertices(string fp, string name, Model model)
     outC.close();
 }
 
-void writeCpositions(string fp, string name, Model model, int faces[][9], float positions[][3])
+void writeCpositions(string fp, string name, Model model, int faces[][10], float positions[][3], int counts[])
 {
     ofstream outC;
     outC.open(fp, ios::app);
@@ -191,15 +217,32 @@ void writeCpositions(string fp, string name, Model model, int faces[][9], float 
     outC << "const float " << name << "Positions[" << model.vertices * 3 << "] =" << endl;
     outC << "{" << endl;
     
-    for (int i = 0; i < model.faces; i++) {
-        int vA = faces[i][0] - 1;
-        int vB = faces[i][3] - 1;
-        int vC = faces[i][6] - 1;
+//    for (int i = 0; i < model.faces; i++) {
+//        int vA = faces[i][0] - 1;
+//        int vB = faces[i][3] - 1;
+//        int vC = faces[i][6] - 1;
+//        
+//        outC << positions[vA][0] << ", " << positions[vA][1] << ", " << positions[vA][2] << ", " << endl;
+//        outC << positions[vB][0] << ", " << positions[vB][1] << ", " << positions[vB][2] << ", " << endl;
+//        outC << positions[vC][0] << ", " << positions[vC][1] << ", " << positions[vC][2] << ", " << endl;
+//    }
+    
+    for (int j = 0; j < model.materials; j++) {
+        counts[j] = 0;
         
-        outC << positions[vA][0] << ", " << positions[vA][1] << ", " << positions[vA][2] << ", " << endl;
-        outC << positions[vB][0] << ", " << positions[vB][1] << ", " << positions[vB][2] << ", " << endl;
-        outC << positions[vC][0] << ", " << positions[vC][1] << ", " << positions[vC][2] << ", " << endl;
+        for (int i = 0; i < model.faces; i++) {
+            if (faces[i][9] == j) {
+                int vA = faces[i][0] - 1;
+                int vB = faces[i][3] - 1;
+                int vC = faces[i][6] - 1;
+                
+                outC << positions[vA][0] << ", " << positions[vA][1] << ", " << positions[vA][2] << ", " << endl;
+                outC << positions[vB][0] << ", " << positions[vB][1] << ", " << positions[vB][2] << ", " << endl;
+                outC << positions[vC][0] << ", " << positions[vC][1] << ", " << positions[vC][2] << ", " << endl;
 
+                counts[j] += 3;
+            }
+        }
     }
     
     
@@ -210,7 +253,7 @@ void writeCpositions(string fp, string name, Model model, int faces[][9], float 
     outC.close();
 }
 
-void writeCtexels(string fp, string name, Model model, int faces[][9], float texels[0][2])
+void writeCtexels(string fp, string name, Model model, int faces[][10], float texels[0][2])
 {
     ofstream outC;
     outC.open(fp, ios::app);
@@ -218,14 +261,28 @@ void writeCtexels(string fp, string name, Model model, int faces[][9], float tex
     outC << "const float " << name << "Texels[" << model.vertices * 2 << "] = " << endl;
     outC << "{" << endl;
     
-    for (int i = 0; i < model.faces; i++) {
-        int vtA = faces[i][1] - 1;
-        int vtB = faces[i][4] - 1;
-        int vtC = faces[i][7] - 1;
-        
-        outC << texels[vtA][0] << ", " <<texels[vtA][1] << ", " <<endl;
-        outC << texels[vtB][0] << ", " <<texels[vtB][1] << ", " <<endl;
-        outC << texels[vtC][0] << ", " <<texels[vtC][1] << ", " <<endl;
+//    for (int i = 0; i < model.faces; i++) {
+//        int vtA = faces[i][1] - 1;
+//        int vtB = faces[i][4] - 1;
+//        int vtC = faces[i][7] - 1;
+//        
+//        outC << texels[vtA][0] << ", " <<texels[vtA][1] << ", " <<endl;
+//        outC << texels[vtB][0] << ", " <<texels[vtB][1] << ", " <<endl;
+//        outC << texels[vtC][0] << ", " <<texels[vtC][1] << ", " <<endl;
+//    }
+    
+    for (int j = 0; j < model.materials; j++) {
+        for (int i = 0; i < model.faces; i++) {
+            if (faces[0][9] == j) {
+                int vtA = faces[i][1] - 1;
+                int vtB = faces[i][4] - 1;
+                int vtC = faces[i][7] - 1;
+                
+                outC << texels[vtA][0] << ", " <<texels[vtA][1] << ", " <<endl;
+                outC << texels[vtB][0] << ", " <<texels[vtB][1] << ", " <<endl;
+                outC << texels[vtC][0] << ", " <<texels[vtC][1] << ", " <<endl;
+            }
+        }
     }
     
     
@@ -235,7 +292,7 @@ void writeCtexels(string fp, string name, Model model, int faces[][9], float tex
     outC.close();
 }
 
-void writeCnormals(string fp, string name, Model model, int faces[][9], float normals[][3])
+void writeCnormals(string fp, string name, Model model, int faces[][10], float normals[][3])
 {
     ofstream outC;
     outC.open(fp, ios::app);
@@ -243,14 +300,178 @@ void writeCnormals(string fp, string name, Model model, int faces[][9], float no
     outC << "const float " << name << "Normals[" << model.vertices * 3 << "] =" << endl;
     outC << "{" << endl;
     
-    for (int i = 0; i < model.faces; i++) {
-        int vnA = faces[i][2] - 1;
-        int vnB = faces[i][5] - 1;
-        int vnC = faces[i][8] - 1;
+//    for (int i = 0; i < model.faces; i++) {
+//        int vnA = faces[i][2] - 1;
+//        int vnB = faces[i][5] - 1;
+//        int vnC = faces[i][8] - 1;
+//        
+//        outC << normals[vnA][0] << ", " << normals[vnA][1] << ", " << normals[vnA][2] << ", " << endl;
+//        outC << normals[vnB][0] << ", " << normals[vnB][1] << ", " << normals[vnB][2] << ", " << endl;
+//        outC << normals[vnC][0] << ", " << normals[vnC][1] << ", " << normals[vnC][2] << ", " << endl;
+//    }
+    
+    for (int j = 0; j < model.materials; j++) {
+        for (int i = 0; i < model.faces; i++) {
+            if (faces[i][9] == j) {
+                int vnA = faces[i][2] - 1;
+                int vnB = faces[i][5] - 1;
+                int vnC = faces[i][8] - 1;
+                
+                outC << normals[vnA][0] << ", " << normals[vnA][1] << ", " << normals[vnA][2] << ", " << endl;
+                outC << normals[vnB][0] << ", " << normals[vnB][1] << ", " << normals[vnB][2] << ", " << endl;
+                outC << normals[vnC][0] << ", " << normals[vnC][1] << ", " << normals[vnC][2] << ", " << endl;
+
+            }
+        }
+    }
+    
+    outC << "};" << endl;
+    outC << endl;
+    
+    outC.close();
+}
+
+int getMTLinfo(string fp)
+{
+    int m = 0;
+    
+    ifstream inMTL;
+    inMTL.open(fp);
+    if (!inMTL.good()) {
+        cout << "ERROR OPENING MTL FILE" << endl;
+        exit(1);
+    }
+    
+    while (!inMTL.eof()) {
+        string line;
+        getline(inMTL, line);
+        string type = line.substr(0, 2);
         
-        outC << normals[vnA][0] << ", " << normals[vnA][1] << ", " << normals[vnA][2] << ", " << endl;
-        outC << normals[vnB][0] << ", " << normals[vnB][1] << ", " << normals[vnB][2] << ", " << endl;
-        outC << normals[vnC][0] << ", " << normals[vnC][1] << ", " << normals[vnC][2] << ", " << endl;
+        if (type.compare("ne") == 0) {
+            m++;
+        }
+    }
+    
+    inMTL.close();
+    return m;
+}
+
+void extractMTLdata(string fp, string *materails, float diffuse[][3], float speculars[][3])
+{
+    int m = 0;
+    int d = 0;
+    int s = 0;
+    
+    ifstream inMTL;
+    inMTL.open(fp);
+    if (!inMTL.good()) {
+        cout << "ERROR OPENING MTL FILE" << endl;
+        exit(1);
+    }
+    
+    while (!inMTL.eof()) {
+        string line;
+        getline(inMTL, line);
+        string type = line.substr(0, 2);
+        
+        if (type.compare("ne") == 0) {
+            string l = "newmtl ";
+            materails[m] = line.substr(l.size());
+            m++;            
+        } else if (type.compare("Kd") == 0) {
+            char *l = new char[line.size() + 1];
+            memcpy(l, line.c_str(), line.size() + 1);
+            
+            strtok(l, " ");
+            for (int i = 0; i < 3; i++) {
+                diffuse[d][i] = atof(strtok(NULL, " "));
+            }
+            
+            delete [] l;
+            d++;
+
+        } else if (type.compare("Ks") == 0) {
+            char *l = new char[line.size() + 1];
+            memcpy(l, line.c_str(), line.size() + 1);
+            
+            strtok(l, " ");
+            for (int i = 0; i < 3; i++) {
+                speculars[s][i] = atof(strtok(NULL, " "));
+            }
+            
+            delete [] l;
+            s++;
+        }
+    }
+    
+    inMTL.close();
+}
+
+void writeCmaterials(string fp, string name, Model model, int firsts[], int counts[])
+{
+    ofstream outC;
+    outC.open(fp, ios::app);
+    
+    outC << "const int " << name << "Materials = " << model.materials << ";" << endl;
+    outC << endl;
+    
+    outC << "const int " << name << "Firsts[" << model.materials << "] = " << endl;
+    outC << "{" << endl;
+
+    for (int i = 0; i < model.materials; i++) {
+        if (i == 0) {
+            firsts[0] = 0;
+        } else {
+            firsts[i] = firsts[i-1] + counts[i-1];
+        }
+        
+        outC << firsts[i] << ", " << endl;
+    }
+    
+    outC << "};" << endl;
+    outC << endl;
+    
+    outC << "const int " << name << "Counts[" << model.materials << "] = " << endl;
+    outC << "{" << endl;
+    
+    for (int i = 0; i < model.materials; i++) {
+        outC << counts[i] << ", " << endl;
+    }
+    
+    outC << "};" << endl;
+    outC << endl;
+    
+    outC.close();
+}
+
+void writeCdiffuses(string fp, string name, Model model, float diffuses[][3])
+{
+    ofstream outC;
+    outC.open(fp, ios::app);
+    
+    outC << "const float " << name << "Diffuses[" << model.materials << "][3] = " << endl;
+    outC << "{" << endl;
+    
+    for (int i = 0; i < model.materials; i++) {
+        outC << diffuses[i][0] << ", " << diffuses[i][1] << ", " << diffuses[i][2] << ", " << endl;
+    }
+    
+    outC << "};" << endl;
+    outC << endl;
+    
+    outC.close();
+}
+
+void writeCspeculars(string fp, string name, Model model, float speculars[][3])
+{
+    ofstream outC;
+    outC.open(fp, ios::app);
+    
+    outC << "const float " << name << "Speculars[" << model.materials << "][3] = " << endl;
+    outC << "{" << endl;
+    
+    for (int i = 0; i < model.materials; i++) {
+        outC << speculars[i][0] << ", " << speculars[i][1] << ", " << speculars[i][2] << ", " << endl;
     }
     
     outC << "};" << endl;
@@ -265,11 +486,14 @@ int main(int argc, const char * argv[]) {
     string filepathOBJ = "source/" + nameOBJ + ".obj";
     string filepathH = "product/" + nameOBJ + ".h";
     string filepathC = "product/" + nameOBJ + ".c";
+    string filepathMTL = "source/" + nameOBJ + ".mtl";
     
     cout << filepathOBJ << endl;
     
     Model model = getOBJinfo(filepathOBJ);
-    
+    model.materials = getMTLinfo(filepathMTL);
+    cout << "Materials: " << model.materials << endl;
+
 //    cout << "Model Info" << endl;
 //    cout << "Positions: " << model.positions << endl;
 //    cout << "Texels: " << model.texels << endl;
@@ -281,9 +505,37 @@ int main(int argc, const char * argv[]) {
     float positions[model.positions][3];
     float texels[model.texels][2];
     float normals[model.normals][3];
-    int faces[model.faces][9];
+    int faces[model.faces][10];
     
-    extractOBJData(filepathOBJ, positions, texels, normals, faces);
+    string *materials = new string[model.materials];
+    float diffuses[model.materials][3];
+    float speculars[model.materials][3];
+    
+    extractMTLdata(filepathMTL, materials, diffuses, speculars);
+    
+//    for (int i = 0; i < model.materials; i++) {
+//        cout << "Name" << i << ": " << materials[i] << endl;
+//        cout << "Kd1: " << diffuses[i][0] << "r " << diffuses[i][1] << "g " << diffuses[i][2] <<
+//        "b " << endl;
+//        cout << "Ks1: " << speculars[i][0] << "r " << speculars[i][1] << "g " << speculars[i][2]
+//        << "b " << endl;
+//    }
+    
+
+    
+    extractOBJData(filepathOBJ, positions, texels, normals, faces, materials, model.materials);
+    
+//    cout << "Material References" << endl;
+//    for (int i = 0; i < model.faces; i++) {
+//        int m = faces[i][9];
+//        cout << "F" << i << "m: " << materials[m] << endl;
+//    }
+    
+//    cout << "Name1: " << materials[0] << endl;
+//    cout << "Kd1: " << diffuses[0][0] << "r " << diffuses[0][1] << "g " << diffuses[0][2] <<
+//    "b " << endl;
+//    cout << "Ks1: " << speculars[0][0] << "r " << speculars[0][1] << "g " << speculars[0][2]
+//    << "b " << endl;
     
 //    cout << "Model Data" << endl;
 //    
@@ -292,11 +544,17 @@ int main(int argc, const char * argv[]) {
 //    cout << "N1: " << normals[0][0] << "x " << normals[0][1] << "y " << normals[0][2] << "z " <<endl;
 //    cout << "F1v1: " << faces[0][0] << "p " << faces[0][1] << "t " << faces[0][2] << "n" <<endl;
     
+    int first[model.materials];
+    int counts[model.materials];
+    
     writeH(filepathH, nameOBJ, model);
     writeCvertices(filepathC, nameOBJ, model);
-    writeCpositions(filepathC, nameOBJ, model, faces, positions);
+    writeCpositions(filepathC, nameOBJ, model, faces, positions, counts);
     writeCtexels(filepathC, nameOBJ, model, faces, texels);
     writeCnormals(filepathC, nameOBJ, model, faces, normals);
+    writeCmaterials(filepathC, nameOBJ, model, first, counts);
+    writeCdiffuses(filepathC, nameOBJ, model, diffuses);
+    writeCspeculars(filepathC, nameOBJ, model, speculars);
     
     return 0;
 }
--- product/cube.h
+++ product/cube.h
@@ -5,8 +5,17 @@
 // Normals: 6
 // Faces: 12
 // Vertices: 36
+// Materials: 6
+
 const int cubeVertices;
 const float cubePositions[108];
 const float cubeTexels[72];
 const float cubeNormals[108];
+
+const int cubeMaterials;
+const int cubeFirsts[6];
+const int cubeCounts[6];
+
+const float cubeDiffuses[6][3];
+const float cubeSpeculars[6][3];
 
